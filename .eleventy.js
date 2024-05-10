55f189583854033b5ba766fa0d3d5da156a9aba9
CCRS Event <ccrsevent@gmail.com>
2024-05-10 15:39:14 +0800

    Init commit

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes", /* Relative to "dir.input" folder */
+      data: "_data", /* Relative to "dir.input" folder */
+      output: "_site"
+    },
+    pathPrefix: "/event/2024-CCRS-VCMconference/"
+  }
+};
