83a9bf28413b14f7ef1485b6288e394ca3416374
CCRS Event <ccrsevent@gmail.com>
2024-05-13 16:51:14 +0800

    Extract index page into liquid template, add speaker data

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,16 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/event/2024-CCRS-VCMconference/assets");
 
+  eleventyConfig.addCollection("2024-ccrs-vcmconference-speakers", function (collectionApi) {
+		return collectionApi
+      .getFilteredByTag("2024-ccrs-vcmconference-speakers")
+      .sort((a, b) => parseInt(a.data.order, 10) - parseInt(b.data.order, 10));
+	});
+
+  eleventyConfig.addFilter("keys", function(value) {
+    return JSON.stringify(Object.keys(value));
+  });
+
   return {
     dir: {
       input: "src",
---
+++ README.md
@@ -0,0 +1,5 @@
+11ty Reference  
+[https://www.11ty.dev/docs/](https://www.11ty.dev/docs/)
+
+Liquid Reference  
+[https://shopify.dev/docs/api/liquid](https://shopify.dev/docs/api/liquid)
