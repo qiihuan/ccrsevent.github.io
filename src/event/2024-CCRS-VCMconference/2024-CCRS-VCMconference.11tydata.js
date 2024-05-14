ad407ef918abe672181cd51d86b1bce361cdcff6
CCRS Event <ccrsevent@gmail.com>
2024-05-14 16:44:25 +0800

    Extract articles into md files, add SEO meta

--- src/event/2024-CCRS-VCMconference/2024-CCRS-VCMconference.11tydata.js
+++ src/event/2024-CCRS-VCMconference/2024-CCRS-VCMconference.11tydata.js
@@ -1,6 +1,14 @@
 module.exports = {
   info: {
     zh: {
+      seo: {
+        url: "https://ccrsevent.github.io/event/2024-CCRS-VCMconference/",
+        title: "2024 TCX & CCRS 國際研討會 &mdash; 自願性碳市場：誠信與永續",
+        description: "全球邁入「排碳有價」時代，臺灣為加速推動碳定價，積極架構國內制度。於此國內制度起步的重要階段，本研討會邀請國內外重要專家學者，聚焦討論自願性碳市場中的誠信與永續議題。",
+        eventStartDate: "2024-05-28T09:00-0800",
+        eventEndDate: "2024-05-28T17:00-0800",
+        coverImage: "https://ccrsevent.github.io/event/2024-CCRS-VCMconference/assets/img/cover.png"
+      },
       sections: {
         speakers: {
           navtitle: "講者資訊",
@@ -79,6 +87,14 @@ module.exports = {
     },
     
     en: {
+      seo: {
+        url: "https://ccrsevent.github.io/event/2024-CCRS-VCMconference/en/",
+        title: "2024 TCX & CCRS International Conference &mdash; Integrity & Sustainability In Voluntary Carbon Market",
+        description: "Taiwan is undertaking the crucial process of establishing its domestic carbon pricing system to strengthen its climate policy. This conference is a unique opportunity for experts and scholars, both at home and abroad, to delve into the issues of integrity and sustainability in the voluntary carbon market. Through knowledge and experience sharing, we aim to ignite a transformative dialogue that will help build stronger foundations for the Taiwanese system.",
+        eventStartDate: "2024-05-28T09:00-0800",
+        eventEndDate: "2024-05-28T17:00-0800",
+        coverImage: "https://ccrsevent.github.io/event/2024-CCRS-VCMconference/assets/img/cover.png"
+      },
       sections: {
         speakers: {
           navtitle: "Speakers",
