a78e26bc96f2f4682cafd9cfd55bd86de1d038a7
CCRS Event <ccrsevent@gmail.com>
2024-05-14 12:41:48 +0800

    Extract text into JSON data, add speaker photos

---
+++ src/event/2024-CCRS-VCMconference/2024-CCRS-VCMconference.11tydata.js
@@ -0,0 +1,160 @@
+module.exports = {
+  info: {
+    zh: {
+      sections: {
+        speakers: {
+          navtitle: "講者資訊",
+          title: "專題講者及與談貴賓",
+          subtitle: "Speakers"
+        },
+        agenda: {
+          navtitle: "大會議程",
+          title: "大會議程",
+          subtitle: "Agenda",
+          footnote: "＊主辦單位保留變更議程之權利，實際議程以大會當日公告為主＊"
+        },
+        articles: {
+          navtitle: "專題特寫",
+          title: "專題特寫",
+          subtitle: "Featured Articles"
+        }
+      },
+      organizers: [
+        {
+          title: "主辦單位",
+          items: [
+            {
+              logoUrl: "assets/img/logo-tcx.png",
+              linkUrl: "https://www.tcx.com.tw/zh/",
+              title: "台灣碳權交易所"
+            },
+            {
+              logoUrl: "assets/img/logo-ccrs.png",
+              linkUrl: "https://ccrs.org.tw/",
+              title: "山大學碳權中心"
+            },
+            {
+              logoUrl: "assets/img/logo-twse.png",
+              linkUrl: "https://www.twse.com.tw/",
+              title: "台灣證券交易所"
+            }
+          ]
+        },
+        {
+          title: "協辦單位",
+          items: [
+            {
+              logoUrl: "assets/img/logo-tdcc.png",
+              linkUrl: "https://www.tdcc.com.tw/",
+              title: "臺灣集中保管結算所"
+            },
+            {
+              logoUrl: "assets/img/logo-tx.png",
+              linkUrl: "https://www.tpex.org.tw/",
+              title: "證券櫃檯買賣中心"
+            },
+            {
+              logoUrl: "assets/img/logo-taifex.png",
+              linkUrl: "https://www.taifex.com.tw/",
+              title: "臺灣期貨交易所"
+            }
+          ]
+        }
+      ],
+      footer: {
+        organizerTitle: "主辦單位",
+        coOrganizerTitle: "協辦單位",
+        officeTitle: "辦公室",
+        officeAddress: "806 高雄市前鎮區復興四路1號二樓",
+        officeTel: "07-5376727",
+        schoolTitle: "研究室",
+        schoolAddress: "804 高雄市鼓山區蓮海路70號",
+        schoolTel: "07-5252000 #5310",
+        contactUsTitle: "線上諮詢服務",
+        copyright: `
+          Copyright © 2024 中山大學碳權研究與服務中心
+          All rights reserved.
+        `
+      }
+    },
+    
+    en: {
+      sections: {
+        speakers: {
+          navtitle: "Speakers",
+          title: "Speakers",
+          subtitle: ""
+        },
+        agenda: {
+          navtitle: "Agenda",
+          title: "Agenda",
+          subtitle: "",
+          footnote: "* The organizer reserves the right to modify the agenda; the actual agenda will be announced on the day of the conference. *"
+        },
+        articles: {
+          navtitle: "Articles",
+          title: "Featured Articles",
+          subtitle: ""
+        }
+      },
+      organizers: [
+        {
+          title: "Organizers",
+          items: [
+            {
+              logoUrl: "assets/img/logo-tcx.png",
+              linkUrl: "https://www.tcx.com.tw/zh/",
+              title: "台灣碳權交易所"
+            },
+            {
+              logoUrl: "assets/img/logo-ccrs.png",
+              linkUrl: "https://ccrs.org.tw/",
+              title: "山大學碳權中心"
+            },
+            {
+              logoUrl: "assets/img/logo-twse.png",
+              linkUrl: "https://www.twse.com.tw/",
+              title: "台灣證券交易所"
+            }
+          ]
+        },
+        {
+          title: "Co-Organizers",
+          items: [
+            {
+              logoUrl: "assets/img/logo-tdcc.png",
+              linkUrl: "https://www.tdcc.com.tw/",
+              title: "臺灣集中保管結算所"
+            },
+            {
+              logoUrl: "assets/img/logo-tx.png",
+              linkUrl: "https://www.tpex.org.tw/",
+              title: "證券櫃檯買賣中心"
+            },
+            {
+              logoUrl: "assets/img/logo-taifex.png",
+              linkUrl: "https://www.taifex.com.tw/",
+              title: "臺灣期貨交易所"
+            }
+          ]
+        }
+      ],
+      footer: {
+        organizerTitle: "Organizer",
+        coOrganizerTitle: "Co-Organizer",
+        officeTitle: "Office",
+        officeAddress: "806 2F., No. 1, Fuxing 4th Rd., Qianzhen Dist., Kaohsiung 806611, Taiwan (R.O.C.)",
+        officeTel: "07-5376727",
+        schoolTitle: "School",
+        schoolAddress: " 804 No. 70, Lianhai Rd., Gushan Dist., Kaohsiung 804201, Taiwan (R.O.C.)",
+        schoolTel: "07-5252000 #5310",
+        contactUsTitle: "Contact Us",
+        copyright: `
+          Copyright © 2024 CCRS
+          All rights reserved.
+        `
+      }
+    }
+  },
+  buildVersion: process.env.BUILD_VERSION || 'dev'
+}
